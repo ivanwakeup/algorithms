78dabac61f578d459356741569a256687b959efc
ivanwakeup <ivan9miller@gmail.com>
2018-11-05 10:08:26 -0700

    Yap

--- algorithms/prep/longest_common_subseq.py
+++ algorithms/prep/longest_common_subseq.py
@@ -1,6 +1,13 @@
 '''
 step1 - define the subproblems:
 
+let i = len(s1)
+let j = len(s2)
+
+if s1[i] and s2[j] match, then LCS = 1 + LCS of rest
+
+LCS(s1, s2) = max( 1 + LCS(s1[:i], s2[:j]), LCS(s1[:i], s2[:j+1]), LCS(s1[:i+1], s2[:j]) )
+= max of 1 + LCS(restofs1, restofs2) if match, or LCS including Ith char or Jth char
 '''
 
 
@@ -8,5 +15,4 @@ word1 = "HIEROGLYPHOLOGY"
 word2 = "MICHAELANGELO"
 
 
-def longest_common_substring(word1, word2):
-    pass
+def longest_common_subsequence(word1, word2):

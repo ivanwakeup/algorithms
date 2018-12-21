5b6d374d5bb3db22c3fcda5f2eb0777de4341688
ivanwakeup <ivan9miller@gmail.com>
2018-12-21 12:39:17 -0700

    c practice

--- c-practice/typecast.c
+++ c-practice/typecast.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-    int a = 5;
+    /*int a = 5;
     int *p = &a;
-    printf("value of p is: %d\n", p);
+    int **b;
+    **b = 3;
+    printf("value of p is: %d\n", *p);
+    printf("value of string is: %s", );*/
+    int a = 5;
+    printf("Value a is : %d\n", a);
+    printf("Address a is : %p\n", &a);
+    int *p;
+    p = &a;
+    printf("Value *p is : %d\n", *p);
+    printf("address p is : %p\n", &p);
+    printf("address p is : %p\n", p+1);
+    printf("address p is : %p\n", p+2);
 }

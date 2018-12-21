9e59c903e5e25bd61803b047d728f0da651bba4e
ivanwakeup <ivan9miller@gmail.com>
2018-12-20 20:49:29 -0700

    yap

---
+++ algorithms/prep/data_structures/hash_map.py
@@ -0,0 +1,24 @@
+
+'''
+need a hashing function to compute bucket within array
+how should hash collisions be resolved?
+
+questions:
+1. how do i know how big of an array i need?
+'''
+class ChainedHashMap:
+
+    class Chain:
+        def __init__(self, val):
+            self.val = val
+            self.next = None
+
+    def __init__(self):
+        self.map_size = 8
+
+        self.map = [self.Chain(None)] * self.map_size
+
+
+    def hash(self, value):
+
+

58fdcd76a1b0e2c134547b870b240fdd5119a64e
ivanwakeup <ivan9miller@gmail.com>
2018-12-24 17:34:24 -0700

    workin

--- algorithms/prep/data_structures/hash_map.py
+++ algorithms/prep/data_structures/hash_map.py
@@ -14,11 +14,41 @@ class ChainedHashMap:
             self.next = None
 
     def __init__(self):
-        self.map_size = 8
+        self.map_size = 7
+        self.keys_stored = 0
 
         self.map = [self.Chain(None)] * self.map_size
 
-
+    '''
+    how do you compute a hash function?
+    i want to take the "hash" value of whatever i supply and mod that by the size of the map to get the correct slot
+    how should we handle increasing the table size?
+    '''
     def hash(self, value):
         pass
+
+    '''
+    let's double the table size each time we need to resize
+    to calculate primes, you only need to check
+    '''
+    @staticmethod
+    def get_next_prime(cur):
+        nxt = cur*2
+        checker = 2
+        while checker < nxt:
+            if not nxt % checker:
+                nxt -= 1
+                checker = 2
+                continue
+            else:
+                checker +=1
+        return nxt
+
+
+print(ChainedHashMap.get_next_prime(37))
+
+
+
+
+
 

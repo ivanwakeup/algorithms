fd6c90ee2e446d8797e209923672023da430edc0
ivanwakeup <ivan9miller@gmail.com>
2019-03-07 17:56:24 -0700

    working brute force LRU

--- algorithms/data_structures/LRU_cache.py
+++ algorithms/data_structures/LRU_cache.py
@@ -14,21 +14,56 @@ PUT and GET operations in constant time?
 
 class LRUCache:
 
-    def __init__(self):
+    def __init__(self, size):
         self.hm = {}
         self.used_queue = []
+        self.space = size
+        self.time = 0
 
+    '''
+    when we call this, we'll need to update the time at which the item we accessed was used, if it exists.
+    '''
     def get(self, key):
         if key in self.hm:
+            self.update_time(key)
             return self.hm[key]
         else:
             return -1
 
     def set(self, key, value):
-        self.hm[key] = value
+        if self.space <= 0:
+            #we need to remove the LRU key from the hm
+            lru = float('inf')
+            del_key = None
+            idx = None
+            for i in range(len(self.used_queue)):
+                if self.used_queue[i][1] < lru:
+                    lru = self.used_queue[i][1]
+                    del_key = self.used_queue[i][0]
+                    idx = i
+            del self.hm[del_key]
+            self.used_queue.pop(idx)
+            self.space+=1
+            self.set(key, value)
+        else:
+            self.hm[key] = value
+            self.used_queue.append([key, self.time])
+            self.space -= 1
+            self.time += 1
+
+    def update_time(self, key):
+        for i in range(len(self.used_queue)):
+            if self.used_queue[i][0] == key:
+                self.used_queue[i][1] = self.time
+                self.time+=1
 
 
-cache = LRUCache()
+
+cache = LRUCache(2)
 
 cache.set(1, 10)
-print(cache.get(1))
+cache.set(2, 20)
+cache.set(3, 30)
+cache.get(2)
+cache.set(4, 40)
+print(cache.get(4))

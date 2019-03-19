ef9edc7171fb70eebb6ad7525a4bf1f995f68023
ivanwakeup <ivan9miller@gmail.com>
2019-03-18 22:11:57 -0600

    text justify

--- algorithms/prep/leetcode_text_justify.py
+++ algorithms/prep/leetcode_text_justify.py
@@ -11,45 +11,87 @@ class Solution:
         i = 0
 
         cur_words = []
+        cur_len = 0
         while i < n:
-            if len(words[i]) + 1 <= remains:
-                remains -= len(words[i]) + 1
-                cur_words.append(words[i] + " ")
+            if cur_len + len(words[i]) + len(cur_words) < maxWidth:
+                cur_words.append(words[i])
+                cur_len += len(words[i])
             else:
-                # tHE LAST WORD OF A LINE WONT HAVE ANY SPACES AFTER IT, just before. so we need to modify the add_leftover_spaces routine
-                justified = self.add_leftover_spaces(cur_words, remains)
+                added_space = [x + " " for x in cur_words]
+                justified = self.justify_line(added_space, maxWidth-cur_len)
                 res.append(justified)
-                remains = maxWidth - (len(words[i]) + 1)
-                cur_words = [words[i] + " "]
+                cur_words = [words[i]]
+                cur_len = len(words[i]) + 1
+
             i += 1
         # now we're on the last line
         if cur_words:
-            line = "".join(cur_words) + (" " * remains)
-            res.append(line)
+            last = self.justify_last(cur_words, remains)
+            res.append(last)
 
         return res
 
-    def add_leftover_spaces(self, line_array, space_count):
-        slots = max(1, len(line_array) - 1)
-        to_add = space_count // slots
-        remaining = space_count - (space_count - to_add)
-        res = []
-        for i in range(len(line_array) - 1):
-            if remaining:
-                res.append(line_array[i] + " " * (to_add + 1))
-            else:
-                res.append(line_array[i] + " " * to_add)
-            remaining -= 1
-        res.append(line_array[-1].strip())
+    def justify_line(self, line_array, spaces):
+        # what if the last word in the line_array ends with a space?
+        if len(line_array) == 1:
+            return line_array[0] + (" "*spaces)
 
-        return "".join(res)
+        result = []
 
+        # calculate remaining spaces to distribute
+        slots = len(line_array) - 1
+        per_slot = spaces // slots
+        remaining = spaces % slots
+
+        for i in range(len(result) - 1):
+            result[i] = result[i] + (" " * per_slot)
+
+        for i in range(len(result) - 1):
+            if remaining > 0:
+                result[i] = result[i] + " "
+            remaining-=1
+
+        return "".join(result)
+
+    def justify_last(self, line, spaces):
+        # if this is the last line, left justify it and return
+        return "".join(line) + (" " * spaces)
 
 sol = Solution()
 
-data = ["This", "is", "an", "example", "of", "text", "justification."]
+data=["What","must","be","acknowledgment","shall","be"]
+
 size = 16
 
 output = sol.fullJustify(data, 16)
 
 print(output)
+
+
+'''
+what information do we need to justify a line of text?
+if there is only one word, it is left justified
+if there are two or more words, we fully justify it
+'''
+def justify_line(line_array, spaces):
+    #what if the last word in the line_array ends with a space?
+    if len(line_array) == 1:
+        return "".join([line_array[0] + (" " * spaces)])
+    result = []
+    for i in range(len(line_array)-1):
+        result.append(line_array[i] + " ")
+        spaces-=1
+    result.append(line_array[-1])
+    #calculate remaining spaces to distribute
+    slots = len(line_array) - 1
+    per_slot = spaces // slots
+    remaining = spaces % slots
+
+    for i in range(len(result)-1):
+        result[i] = result[i] + (" "*per_slot)
+
+    for i in range(len(result)-1):
+        if remaining>0:
+            result[i] = result[i] + " "
+
+    return result

e586e7c33c12ab149590ed11dbfc73bbb802f463
ivanwakeup <ivan9miller@gmail.com>
2019-04-04 15:28:06 -0600

    fuck palindrome pairs

--- algorithms/data_structures/palindrome_pairs_trie.py
+++ algorithms/data_structures/palindrome_pairs_trie.py
@@ -78,21 +78,70 @@ as nested paths, we also store whether WORD(i, j) is a palindrome. that is, whet
 
 this class should be initialized with an array of words to store in the trie.
 '''
+from collections import defaultdict
 class PPTrie:
 
-    paths = {}
+    def __init__(self):
+        self.paths = defaultdict(PPTrie)
+        self.is_word_idx = -1
+        self.palins_below = []
 
-    def __init__(self, input_words):
-        for idx, word in enumerate(input_words):
-            self.add_word(word, idx)
+    @staticmethod
+    def add_word(trie, word, idx):
+        word = word[::-1]
+        for i, char in enumerate(word):
+            rev = word[::-1]
+            is_palin_below = PPTrie.is_palindrome(rev[0:len(word)-i])
+            if is_palin_below:
+                trie.palins_below.append(idx)
+            trie = trie.paths[char]
+        trie.is_word_idx = idx
 
-    def add_word(self, word, idx):
-        pass
+    @staticmethod
+    def is_palindrome(word):
+        if len(word) == 0 or len(word) == 1:
+            return True
+        return word[0] == word[-1] and PPTrie.is_palindrome(word[1:-1])
 
-    def is_palindrome(self, word, i, j):
-        while i < j:
-            if word[i] != word[j]:
-                return False
-            i+=1
-            j-=1
-        return True
+def build_trie(words):
+    top_trie = PPTrie()
+    for i, word in enumerate(words):
+        PPTrie.add_word(top_trie, word,i)
+    return top_trie
+
+
+
+'''
+search for each word in the trie.
+if we encounter trie.is_word_idx, check if the remainder of the current word is a palindrome. if so, append its index as well as the is_word_idx to the result
+additionally, append pairs of cur_word_idx and palins_below[i] for each i in palins_below
+
+'''
+def get_word_palins(trie, word):
+    result = []
+    for i, char in enumerate(word):
+        #handling the case for the "" character. it would "form a word" at the root
+        if trie.is_word_idx >= 0:
+            if PPTrie.is_palindrome(word[0:len(word)-i]):
+                result.append(trie.is_word_idx)
+        if char not in trie.paths:
+            return result
+        trie = trie.paths[char]
+    if trie.is_word_idx >= 0:
+        result.append(trie.is_word_idx)
+    result.extend(trie.palins_below)
+    return result
+
+def palindrome_pairs(words):
+
+    pt = build_trie(words)
+    result = []
+    for i, word in enumerate(words):
+        palins = get_word_palins(pt, word)
+        for idx in palins:
+            if idx != i:
+                result.append([i, idx])
+    return result
+
+
+print(palindrome_pairs(["abcd","dcba","lls","s","sssll"]))
--- algorithms/prep/palindrome_pairs.py
+++ algorithms/prep/palindrome_pairs.py
@@ -1,89 +1,73 @@
-'''
-
-edge cases:
-1. need to check in TRIE that the word we're trying to find isn't the same index as the one we're currently considering
-2. once we find the PREFIX for a string in the TRIE:
-    1. it may have multiple suffixes, need to check if any of them are palindromes. so:
-        for each LETTER in the next level of the trie, check if the string INDEXED by that trie is a palindrome between
-        the index that LETTER appears at and the start of the string.
-
-    2. we may have found a potential SUFFIX in the array, rather than a prefix. if this is the case,
-        we need to check that the remainder of the string we're checking is also a palindrome.
-
-
-what to store in TrieNode:
-(letter, word_idx_array, char_idx_array)
-
-
-approach:
-add reverse of each word to Trie, storing (letter, word_idx_array, char_idx_array)
-iterate through WORDS. For each CHAR in WORD:
-    if CHAR in TRIE:
-        store index I of CHAR
-        keep finding CHAR in TRIE if it exists. if it doesnt, we can't form a palindrome pair so continue
-        if we reach the end of the WORD, for each word in TRIE[WORD_IDX_ARRAY], can we form palindrome between word start and CHAR_IDX_ARRAY-1?
-        if we don't reach end of WORD, did we find a * in trie and the rest of WORD is palindrome?
-
-'''
-class Solution(object):
+class Solution:
     def palindromePairs(self, words):
-        top_trie = {}
-        for i in range(len(words)):
-            self.build_trie(top_trie, words[i][::-1], i)
-        result = []
-        for s in range(len(words)):
-            trie = top_trie
-            word = words[s]
-            for i in range(len(word)):
-                if not word[i] in trie:
-                    break
+        trie = makeTrie(words)
+        output = []
+        for i, word in enumerate(words):
+            candidates = self.getPalindromesForWord(trie, word)
+            # this is how we don't add duplicates to result
+            output.extend([[i, c] for c in candidates if i != c])
+        return output
 
-                #need to make sure the index of this letter in the trie ISNT THE SAME as the one we're checking
-                if len(trie[word[i]][1])==1 and trie[word[i]][1][0] == s:
-                    break
+    def getPalindromesForWord(self, trie, word):
 
-                if i == len(word)-1:
-                    for j, idx in enumerate(trie[word[i]][1]):
-                        to_check = words[idx]
-                        start = trie[word[i]][2][j]
-                        if self.is_palindromic(to_check, 0, start):
-                            result.append([s, idx])
-                            break
+        output = []
+        while word:
+            if trie.wordEndIndex >= 0:
+                if isPalindrome(word):
+                    output.append(trie.wordEndIndex)
+            if not word[0] in trie.paths:
+                return output
+            trie = trie.paths[word[0]]
+            word = word[1:]
 
-                trie = trie[word[i]][0]
-                if "*" in trie and trie["*"][1][0] != s:
-                    if self.is_palindromic(word, i+1, len(word)-1):
-                        idx = trie["*"][1][0]
-                        result.append([s, idx])
-
-        return result
+        if trie.wordEndIndex >= 0:
+            output.append(trie.wordEndIndex)
+        output.extend(trie.palindromesBelow)
+        return output
 
 
-    '''
-    is the string palindromic between two indicies?
-    '''
-    def is_palindromic(self, s, l, h):
-        if not s:
-            return True
-        while l < h:
-            if s[l] != s[h]:
-                return False
-            l+=1
-            h-=1
+from collections import defaultdict
+
+
+class Trie:
+    def __init__(self):
+        # letter -> next trie node.
+        self.paths = defaultdict(Trie)
+        # If a word ends at this node, then this will be a positive value
+        # that indicates the location of the word in the input list.
+        self.wordEndIndex = -1
+        # Stores all words that are palindromes from this node to end of word.
+        # e.g. if we are on a path 'a','c' and word "babca" exists in this trie
+        # (words are added in reverse), then "acbab"'s index will be in this
+        # list since "bab" is a palindrome.
+        self.palindromesBelow = []
+
+    # Adds a word to the trie - the word will be added in
+    # reverse (e.g. adding abcd adds the path d,c,b,a,$index) to the trie.
+    # word - string the word to be added
+    # index - int index of the word in the list, used as word identifier.
+    def addWord(self, word, index):
+        trie = self
+        for j, char in enumerate(reversed(word)):
+            if isPalindrome(word[0:len(word) - j]):
+                trie.palindromesBelow.append(index)
+            trie = trie.paths[char]
+        trie.wordEndIndex = index
+
+
+def makeTrie(words):
+    trie = Trie()
+    for i, word in enumerate(words):
+        trie.addWord(word, i)
+    return trie
+
+
+def isPalindrome(word):
+    if len(word) == 0 or len(word) == 1:
         return True
-
-    def build_trie(self, trie_start, word, word_idx):
-        for i in range(len(word)):
-            if word[i] in trie_start:
-                trie_start[word[i]][1].append(word_idx)
-                trie_start[word[i]][2].append(i)
-            else:
-                trie_start[word[i]] = ({}, [word_idx], [i])
-            trie_start = trie_start[word[i]][0]
-        trie_start["*"] = (None, [word_idx], -1)
-
+    return word[0] == word[-1] and isPalindrome(word[1:-1])
 
 
 sol = Solution()
 
-print(sol.palindromePairs(["lls","s","sssll"]))
+sol.palindromePairs(["abcd","dcba","lls","s","sssll"])

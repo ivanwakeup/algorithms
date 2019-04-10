0934e845e3b31d3a008b4bd57814b946cca39f24
ivanwakeup <ivan9miller@gmail.com>
2019-04-09 19:24:36 -0600

    add hash set

--- algorithms/data_structures/hash_map.py
+++ algorithms/data_structures/hash_map.py
@@ -1,52 +1,101 @@
-
 '''
-need a hashing function to compute bucket within array
-how should hash collisions be resolved?
+implement a hash map
 
-questions:
-1. how do i know how big of an array i need?
+needs to:
+
+add_key 0(1) expected, 0(n) worst case
+delete_key
+lookup_key
+
+
+approach:
+need to compute a hash for keys to be added
+mod that hash value by
+
+
+
+considerations:
+when do we increase the size of the array storing the values?
+we could do it every time the occupied space > half the total space
+
+the probability of a hash collision is about 50% when the table is half full
 '''
-class ChainedHashMap:
 
-    class Chain:
-        def __init__(self, val):
-            self.val = val
-            self.next = None
+
+class LinkedNode:
+
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+    def __repr__(self):
+        return self.data
+
+    def __eq__(self, other):
+        return other == self.data
+
+
+class LinkedHashSet:
 
     def __init__(self):
-        self.map_size = 7
-        self.keys_stored = 0
+        self.size = 7
+        self.table = [LinkedNode("*") for _ in range(self.size)]
+        self.occupied = 0
 
-        self.map = [self.Chain(None)] * self.map_size
+    def add_key(self, key):
+        if self.occupied >= self.size//2:
+            self.add_size()
+        bucket = hash(key)%self.size
+        head = self.table[bucket]
+        prev = LinkedNode("dummy")
+        while head:
+            if head == key:
+                return
+            prev = head
+            head = head.next
+        prev.next = LinkedNode(key)
+        self.occupied+=1
 
-    '''
-    how do you compute a hash function?
-    i want to take the "hash" value of whatever i supply and mod that by the size of the map to get the correct slot
-    how should we handle increasing the table size?
-    '''
-    def hash(self, value):
-        pass
+    def add_size(self):
+        self.size *= 2
+        self.table = self.table + [LinkedNode("*") for _ in range(self.size)]
 
-    '''
-    let's double the table size each time we need to resize
-    to calculate primes, you only need to check
-    '''
-    @staticmethod
-    def get_next_prime(cur):
-        nxt = cur*2
-        checker = 2
-        while checker < nxt:
-            if not nxt % checker:
-                nxt -= 1
-                checker = 2
-                continue
+
+    def delete_key(self, key):
+        bucket = hash(key) % self.size
+        head = self.table[bucket]
+
+        prev = head
+        deleted = False
+        while head:
+            if head == key:
+                tmp = head.next
+                prev.next = tmp
+                deleted = True
+                break
             else:
-                checker +=1
-        return nxt
+                prev = head
+                head = head.next
+        if not deleted:
+            raise ValueError("delete key doesn't exist")
+        self.occupied-=1
+
+    def key_exists(self, key):
+        bucket = hash(key) % self.size
+        head = self.table[bucket]
+        while head:
+            if head == key:
+                return True
+            else:
+                head = head.next
+        return False
 
 
-print(ChainedHashMap.get_next_prime(37))
 
+hm = LinkedHashSet()
+hm.add_key(1)
+hm.delete_key(1)
+print(hm.occupied)
 
 
 

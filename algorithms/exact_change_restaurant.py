f86cc48472fcfdc25e328fa402d463cd8a5ed081
ivanwakeup <ivan9miller@gmail.com>
2019-04-14 21:07:08 -0600

    coding dynamic programming and backtracking

---
+++ algorithms/exact_change_restaurant.py
@@ -0,0 +1,44 @@
+'''
+given a dictionary of menu items and their prices:
+ex {'fruit': 2:15, 'fries': 2.75}
+AND a target price, tp=15.05
+
+find all combinations of the menu items that sum to that target price. WE CAN USE A MENU ITEM MORE THAN ONCE
+
+
+can we do this with dynamic programming?
+
+overlapping subproblems - ?
+
+optimal substructure - we can use the result of subproblem answers to find our main problem answer
+    if we have TP left, and a static dictionary of items/prices, we know whether or not we can make any combinations
+
+    in other words:
+        let fd = food/price dictionary
+        let tp = target price
+        f(fd, tp) = the combinations we can achieve from our FD with TP remaining
+
+        f(fd, tp) = cur_item + f(fd, tp-cur_item_price) for all cur_items
+
+        base case:
+            if tp == 0, append cur list of items to our result
+
+
+
+this really seems like more of a greedy/backtracking algorithm than a dynamic programming one.
+
+
+
+why isn't this dynamic programming?
+
+1. because we're not maximizing/minimizing anything. we're just finding all combinations that add up to some amount
+2. it's different from knapsack, because thats MAXIMIZING A value by picking items to bring along with you
+3. this fits much better with a greedy "grab as many items as you can" approach, and see if we can make exact change
+
+
+we could rephrase this question, perhaps, if we had an "enjoyment" factor associated with a food item
+and we were trying to maximize our enjoyment given a dict of food/prices. but then this really just becomes
+an example of knapsack again.
+
+'''
+
---
+++ algorithms/prep/dynamic_programming/knapsack.py
@@ -0,0 +1,23 @@
+'''
+given two equal-length arrays, ITEMS and VALUES, where each index i represents an (ITEM, VALUE) pair
+
+AS WELL AS
+
+a total_capacity in your knapsack available, tc = 15
+
+what is the maximum VALUE you can achieve by taking items in your knapsack?
+
+
+how does this exhibit:
+
+optimal substructure - we can use subproblems to get the answer to our main problem
+
+let MV(tc, items) be the max value achieveable with a total capacity (tc) and items
+
+MV(tc, items, values) = max {
+    values[i] + MV(tc - items[i], items, values)
+} for all i in items/values
+
+is this true?
+
+'''

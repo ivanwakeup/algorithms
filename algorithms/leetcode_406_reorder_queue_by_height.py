5f57363cdef249bd06a53c8e7118721d54053380
Ivan Miller <ivanwakeup@Ivans-MacBook-Pro.local>
2019-11-29 15:50:27 -0700

    yap

--- algorithms/leetcode_406_reorder_queue_by_height.py
+++ algorithms/leetcode_406_reorder_queue_by_height.py
@@ -1,4 +1,4 @@
-def insert(self, queue, item):
+def insert(queue, item):
     pos = 0
 
     count, val = item
@@ -22,8 +22,8 @@ def insert(self, queue, item):
     else:
         queue.insert(pos, [val, count])
 
-
-def reconstructQueue(self, people):
+import heapq
+def reconstructQueue(people):
     """
     :type people: List[List[int]]
     :rtype: List[List[int]]
@@ -39,9 +39,32 @@ def reconstructQueue(self, people):
     while len(items) != 0:
         item = heapq.heappop(items)
 
-        self.insert(queue, item)
+        insert(queue, item)
 
     return queue
 
 
-input = [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+input = [[7,0], [4,4], [7,1], [6,1], [5,2], [5,0]]
+
+output = reconstructQueue(input)
+print(output)
+
+'''
+2i + 1 <= i
+2i + 2 <= i
+
+'''
+def validate_min_heap(arr, i):
+    if i >= len(arr) or len(arr) == 1:
+        return True
+    left_v = True
+    right_v = True
+    if 2*i + 1 < len(arr):
+        left_v = arr[i] <= arr[2*i + 1] and validate_min_heap(arr, 2*i + 1)
+    if 2*i + 2 < len(arr):
+        right_v = arr[i] <= arr[2*i + 2] and validate_min_heap(arr, 2*i + 2)
+
+    return left_v and right_v
+
+res = validate_min_heap([(0, 7), (1, 6), (0, 5), (4, 4), (2, 5), (1, 7)], 0)
+print(res)

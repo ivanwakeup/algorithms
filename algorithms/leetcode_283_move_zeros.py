8c9f297388785d1b02463c47b6c8aac9aad6af43
ivanwakeup <ivan9miller@gmail.com>
2019-12-07 09:55:37 -0700

    add 283 move zeros

--- algorithms/leetcode_283_move_zeros.py
+++ algorithms/leetcode_283_move_zeros.py
@@ -1,19 +1,19 @@
-def moveZeroes(nums):
+def moveZeroes(nums) -> None:
     """
     Do not return anything, modify nums in-place instead.
     """
     lo = 0
     hi = 1
-    while lo < len(nums) and hi < len(nums):
+    while hi < len(nums):
 
         while lo < len(nums) - 1 and nums[lo] != 0:
             lo += 1
-
-        while hi < len(nums) - 1 and nums[hi] == 0:
+            hi += 1
+        while hi < len(nums) and nums[hi] == 0:
             hi += 1
 
-        nums[lo], nums[hi] = nums[hi], nums[lo]
-        hi+=1
+        if lo < len(nums) and hi < len(nums):
+            nums[lo], nums[hi] = nums[hi], nums[lo]
 
 
 data = [0,1,0,3,12]

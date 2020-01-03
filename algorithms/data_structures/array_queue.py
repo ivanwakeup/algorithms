804ace56775f1bcacd0fc0b6c756ea0aaa4d9f7e
ivanwakeup <ivan9miller@gmail.com>
2020-01-02 20:48:35 -0700

    add array queue implementation

---
+++ algorithms/data_structures/array_queue.py
@@ -0,0 +1,78 @@
+'''
+lets implement a queue with an array.
+
+
+[1,2,3,4,5] -> append on the left side, pop on the right?
+we could initialize an empty array and double it in size every time the queue grows.
+[null, null, 1]
+        ^
+[3,2,1]
+ ^
+
+array full, so add some slots
+[null,null,null,3,2,1]
+             ^
+now pop() -> return 1, space becomes null
+
+[null,null,null,3,2,null]
+             ^     ^
+
+
+as we pop from the right, how do we reclaim the array slots that are now NULL?
+keep track of the array size, and let it be circular
+
+keep a pop and a push pointer that says where to pop and push from, both move to the left
+
+'''
+
+
+class Queue:
+
+    def __init__(self):
+        self.arr = [NoEntry] * 2
+        self.size = 0
+        self.popptr = -1
+        self.pushptr = -1
+
+    def push(self, item):
+        if self.size >= len(self.arr):
+            self.resize()
+        self.arr[self.pushptr] = item
+        self.size += 1
+        self.pushptr = self.calc_ptr_pos(self.pushptr)
+
+    def pop(self):
+        if not self.size:
+            return None
+        item = self.arr[self.popptr]
+        self.popptr = self.calc_ptr_pos(self.popptr)
+        self.size-=1
+        return item
+
+    def calc_ptr_pos(self, ptr):
+        nxt = -1 + ptr
+        return nxt % len(self.arr)
+
+    def resize(self):
+        self.arr = [NoEntry] * len(self.arr) + self.arr
+
+
+class NoEntry:
+    pass
+
+
+q = Queue()
+q.push(1)
+q.push(2)
+q.push(3)
+q.push(3)
+q.push(2)
+q.push(1)
+print(q)
+print(q.pop())
+print(q.pop())
+print(q.pop())
+print(q.pop())
+print(q.pop())
+print(q.pop())
+print(q.pop())

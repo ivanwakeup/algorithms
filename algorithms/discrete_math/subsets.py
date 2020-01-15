875d72ce77e6452fff30ca3cc2ccef118204959c
Ivan Miller <ivanwakeup@Ivans-MacBook-Pro.local>
2020-01-15 12:33:28 -0700

    subsets yo

---
+++ algorithms/discrete_math/subsets.py
@@ -0,0 +1,43 @@
+'''
+wow this one is like fucking mind boggling.
+
+INTUITION:
+we can use a "base" subset as the start for adding the next element we're considering
+to everything that already exists.
+
+for example, if we start with the empty subset {}:
+how many subsets can we make once we add the element 1? Well, just two:
+{}
+and
+{1}
+
+now when we have those two subsets, we can add any ADDITIONAL element to any existing subsets we have to form
+all of the possible subsets with that additional element. So, add 2 and we have:
+{}
+{1}
+{2}
+{1,2}
+
+we can continue on like this to create every possible subset for further elements we want to add.
+'''
+def subsets(nums):
+    result = [[]]
+    for num in nums:
+        tmp = result.copy()
+        for item in result:
+            tmp.append([num] + item)
+        result = tmp
+    return result
+
+
+data = [
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+print(subsets([1,2,3]))

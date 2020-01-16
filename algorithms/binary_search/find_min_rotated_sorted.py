373c9ef56bb747621f0007c0f746b2b8539ad620
ivanwakeup <ivan9miller@gmail.com>
2020-01-15 20:33:43 -0700

    add some binary search algorithms

---
+++ algorithms/binary_search/find_min_rotated_sorted.py
@@ -0,0 +1,45 @@
+'''
+given a rotated sorted array, find the number of times the array is rotated!
+
+ex:
+
+arr = [8.9.10.2.5.6] ans = rotated 3 times
+arr = [2,5,6,8,9,10] ans = rotated 0 times
+
+
+intuition:
+if the array is sorted (but rotated some number of times), we will have 1 element that is less than both of its neighbors!
+using this fact, we know we've found the min when we find an element i such that i-1 > i < i + 1
+so we can use the same approach as binary search to find a given target in a rotated sorted array.
+
+'''
+
+import logging
+log = logging.getLogger()
+
+
+def find_num_rotations(arr):
+    if not arr or len(arr) == 1:
+        return 0
+    lo, hi = 0, len(arr)-1
+    while lo<= hi:
+        mid = (lo+hi)//2
+        prev,next = (mid-1)%len(arr), (mid+1)%len(arr)
+        if prev > mid < next:
+            return mid
+        elif arr[mid] > arr[hi]:
+            lo = mid + 1
+        else:
+            hi = mid - 1
+    return lo
+
+
+data = [([8,9,10,2,5,6], 3), ([2,5,6,8,9,10], 0), ([1,2], 0), ([2,3,1], 2), ([8,9,10,1,2], 3), ([3,4,1], 2)]
+
+
+for arr, expected in data:
+    try:
+        assert find_num_rotations(arr) == expected
+    except AssertionError:
+        log.error((f"{arr} did not return expected result of {expected}"))
+        raise
--- algorithms/binary_search/rotated_sorted_binary.py
+++ algorithms/binary_search/rotated_sorted_binary.py
@@ -1,28 +1,51 @@
+'''
+the idea is actually pretty straightforward:
+
+do a modified binary search.
+
+if we check arr[low] <= arr[mid], we KNOW for certain the rotation must occur in the right half.
+
+THEN, (assuming arr[low] <= arr[mid]) if we just check that our TARGET is between arr[low] and arr[mid], we know to update our HIGH for next round to be
+high = mid -1
+
+if our predicate (arr[low] <= arr[mid]) isn't true, then the rotation must be in the left half.
+
+so we just check the opposite case (is our target between mid and high, if so, search there) and search the appropriate half accordingly
+'''
+
+'''
+perform a regular binary search
+but once we calculate mid, if its the target return
+if not, check if the value is between mid and high -> search there
+else search low
+
+
+[6,7,1,2,3]
+'''
+
+
 class Solution:
     def search(self, nums, target):
+        if not nums:
+            return -1
+        lo, hi = 0, len(nums) - 1
 
-        def do_find(nums, target, cur):
-            if not nums:
-                return -1
-            mid = len(nums) // 2
+        while lo <= hi:
+            mid = (lo + hi) // 2
             if nums[mid] == target:
-                return cur + mid
-            elif nums[mid] < target:
-                if mid + 1 < len(nums) and nums[mid + 1] <= target:
-                    return do_find(nums[mid+1:], target, mid+1)
-                if mid - 1 >= 0 and nums[mid - 1] >= target:
-                    return do_find(nums[:mid], target, 0)
-            else :
-                if mid - 1 >= 0 and nums[mid-1] >= target:
-                    return do_find(nums[:mid], target, cur)
-                if mid + 1 < len(nums) and nums[mid + 1] <= target:
-                    return do_find(nums[mid + 1:], target, mid + 1)
-
-        return do_find(nums, target, 0)
-
-
+                return mid
+            elif target < nums[mid]:
+                if nums[mid] > nums[hi] >= target:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+            else:
+                if nums[mid] < nums[lo] <= target:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+        return -1
 
 
 sol = Solution()
-
-print(sol.search([3,5,1], 3))
+print(sol.search([4,5,6,7,0,1,2], 0))

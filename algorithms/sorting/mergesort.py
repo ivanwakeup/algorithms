326ba7494ba8c29194cba516f4295e2d75e4b585
ivanwakeup <ivan9miller@gmail.com>
2020-01-19 18:23:22 -0700

    adding sorts

--- algorithms/sorting/mergesort.py
+++ algorithms/sorting/mergesort.py
@@ -1,35 +1,137 @@
-def create_array(size=10, max=50):
-    from random import randint
-    return [randint(0,max) for _ in range(size)]
+# def create_array(size=10, max=50):
+#     from random import randint
+#     return [randint(0,max) for _ in range(size)]
+#
+#
+# def merge(a, b):
+#     c = []
+#
+#     a_idx, b_idx = 0,0
+#
+#     while a_idx < len(a) and b_idx < len(b):
+#         if a[a_idx] < b[b_idx]:
+#             c.append(a[a_idx])
+#             a_idx = a_idx + 1
+#         else:
+#             c.append(b[b_idx])
+#             b_idx = b_idx + 1
+#     if a_idx == len(a):
+#         c.extend(b[b_idx:])
+#     else:
+#         c.extend(a[a_idx:])
+#     return c
+#
+#
+# def merge_sort(array):
+#
+#     if len(array) <= 1:
+#         return array
+#
+#     left, right = merge_sort(array[:len(array)/2]), merge_sort(array[len(array)/2:])
+#
+#     return merge(left, right)
+#
+# a = create_array()
+# print(merge_sort(a))
 
 
-def merge(a, b):
-    c = []
 
-    a_idx, b_idx = 0,0
 
-    while a_idx < len(a) and b_idx < len(b):
-        if a[a_idx] < b[b_idx]:
-            c.append(a[a_idx])
-            a_idx = a_idx + 1
+'''
+lets write merge sort!
+
+the key idea is the merge routine, which takes two sorted arrays and merges them together.
+
+[1]
+[2]
+
+pretty straightforward with 2 pointers!
+
+'''
+
+def merge(a1, a2):
+    i = 0
+    j = 0
+    res = []
+    while i < len(a1) and j < len(a2):
+        if a1[i] < a2[j]:
+            res.append(a1[i])
+            i+=1
         else:
-            c.append(b[b_idx])
-            b_idx = b_idx + 1
-    if a_idx == len(a):
-        c.extend(b[b_idx:])
-    else:
-        c.extend(a[a_idx:])
-    return c
+            res.append(a2[j])
+            j+=1
+    while i < len(a1):
+        res.append(a1[i])
+        i+=1
+    while j < len(a2):
+        res.append(a2[j])
+        j+=1
+    return res
+
+def mergesort(arr, lo, hi):
+    if lo == hi:
+        return [arr[lo]]
+    mid = (lo+hi)//2
+    l = mergesort(arr, lo, mid)
+    r = mergesort(arr, mid+1, hi)
+
+    return merge(l, r)
+
+data = [7,4,2,0,1,2,6,4]
+
+print(mergesort(data, 0, len(data)-1))
 
 
-def merge_sort(array):
 
-    if len(array) <= 1:
-        return array
 
-    left, right = merge_sort(array[:len(array)/2]), merge_sort(array[len(array)/2:])
 
-    return merge(left, right)
 
-a = create_array()
-print(merge_sort(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- algorithms/sorting/quicksort.py
+++ algorithms/sorting/quicksort.py
@@ -1,19 +1,105 @@
-import random
-def partition(arr, lo, hi):
-    piv = random.randint(lo, hi)
-    print("using pivot {}".format(piv))
-    arr[piv],arr[hi] = arr[hi],arr[piv]
-    i = lo-1
-    j = lo
-    while j < hi:
+'''
+partition function:
+
+we want to partition an array by choosing some pivot point K, and placing all elements LESS than the pivot to the left of it
+and all elements GREATER than the pivot to the right of it.
+
+
+intuition:
+use an approach that creates two windows from the beginning of the array
+
+the key here is that our LOW pointer represents the end of the smaller_than window.
+the hi pointer represents the section of the array that we're currently considering.
+
+'''
+
+def partition(arr, piv, lo, hi):
+    if not arr:
+        return
+    i = lo - 1
+    #this just keeps the routine easier by letting our piv stay at the end
+    #then we can just swap it out at the end
+    arr[piv], arr[hi] = arr[hi], arr[piv]
+    for j in range(lo, hi):
         if arr[j] < arr[hi]:
             i+=1
-            arr[j], arr[i] = arr[i], arr[j]
-        j+=1
-    arr[hi], arr[i+1] = arr[i+1], arr[hi]
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[hi] = arr[hi], arr[i+1]
 
 
-data = [4,7,3,1,9,8,5,0,1]
-partition(data, 1, 4)
+
+data = [6,9,4,1,7,8]
+
+
+import random
+
+def quicksort(arr):
+
+    def do_quicksort(arr, lo, hi):
+        print(arr)
+        if lo == hi:
+            return
+        mid = (lo + hi)//2
+        piv = random.randint(lo, hi)
+        #this partitioning scheme always leaves the element
+        partition(arr, piv, lo, hi)
+        do_quicksort(arr, mid + 1, hi)
+        do_quicksort(arr, lo, mid)
+
+    do_quicksort(arr, 0, len(arr)-1)
+
+
+quicksort(data)
 print(data)
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
